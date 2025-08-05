4cd1923818c0eb9e3b5a3f8cb1b9d4edf4f39abe
Udit Ranjan <101802436+Phonix10@users.noreply.github.com>
2025-08-05 16:21:02 +0530

    added entity layer where user as java object and usertype as enum

---
+++ src/main/java/com/example/Security_learning/Entity/User.java
@@ -0,0 +1,36 @@
+package com.example.Security_learning.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserType userType;
+
+    
+
+    
+}
---
+++ src/main/java/com/example/Security_learning/Entity/UserType.java
@@ -0,0 +1,6 @@
+package com.example.Security_learning.Entity;
+
+public enum UserType {
+    ADMIN, USER,GUEST;
+    
+}

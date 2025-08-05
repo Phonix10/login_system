99449b8a68ffceede196d94da0e83e9b13c4a12e
Udit Ranjan <101802436+Phonix10@users.noreply.github.com>
2025-08-05 15:52:49 +0530

    adding project in git repo

---
+++ src/test/java/com/example/Security_learning/SecurityLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Security_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
